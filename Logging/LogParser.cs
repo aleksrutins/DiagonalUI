6f888d9e42c42b8a2b39f674e907a193d853173a
Aleks Rūtiņš <aleks@rutins.com>
2022-12-28 11:20:32 -0500

    Add LogParser

---
+++ Logging/LogParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DiagonalUI.Logging
+{
+    class Line
+    {
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+    public delegate void LineHandler(object data);
+    internal class LogParser
+    {
+        private readonly Dictionary<string, LineHandler> _handlers = new();
+
+        private readonly string _tag;
+        public LogParser(string tag)
+        {
+            _tag = tag;
+        }
+
+        public void AddHandler(string messageName, LineHandler handler)
+        {
+            _handlers.Add(messageName, handler);
+        }
+
+        public void ProcessLine(string line)
+        {
+            var index = line.IndexOf("/* log:tag:" + _tag + " */");
+            if (index != -1)
+            {
+                var messageData = line.Substring(index, line.Length - 1);
+                var deserialized = JsonSerializer.Deserialize<Line>(messageData);
+                if(deserialized != null && _handlers.ContainsKey(deserialized.Message))
+                {
+                    _handlers.GetValueOrDefault(deserialized.Message)(deserialized.Data);
+                }
+            }
+        }
+    }
+}
