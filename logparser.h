13addea424622e9d61f325ee36382a89b93c280c
Aleks Rutins <aleks@rutins.com>
2023-01-03 10:38:11 -0500

    Remove window statusbar

--- logparser.h
+++ logparser.h
@@ -13,7 +13,7 @@ class LogParser
     std::string tag;
     std::unordered_map<std::string, std::function<void(QJsonValue)>> handlers;
 public:
-    LogParser(std::string tag) : handlers(), tag(tag) {}
+    LogParser(std::string tag) : tag(tag), handlers() {}
     void addHandler(std::string name, std::function<void(QJsonValue)> handler);
     void processMessage(std::string message);
 };
